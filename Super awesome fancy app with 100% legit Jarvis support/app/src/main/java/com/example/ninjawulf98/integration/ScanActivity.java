952e8d0f11c5c9a6b48998fa5a2c3287c17c7682
Unknown <ninjawulf98@gmail.com>
2017-12-18 15:24:14 +0100

    hello there

--- Super awesome fancy app with 100% legit Jarvis support/app/src/main/java/com/example/ninjawulf98/integration/ScanActivity.java
+++ Super awesome fancy app with 100% legit Jarvis support/app/src/main/java/com/example/ninjawulf98/integration/ScanActivity.java
@@ -3,11 +3,11 @@ package com.example.ninjawulf98.integration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LampTest extends AppCompatActivity {
+public class ScanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lamp_tet);
+        setContentView(R.layout.activity_scan);
     }
 }

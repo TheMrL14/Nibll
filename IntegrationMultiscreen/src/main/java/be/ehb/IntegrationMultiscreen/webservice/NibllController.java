79a805438acdf75fda6fc516a5ed3520c391b4ab
Unknown <ninjawulf98@gmail.com>
2017-12-19 15:11:10 +0100

    f

--- IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 //import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,6 +31,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  * @author Lennert
  */
+@RestController
+@RequestMapping("test")
 public class NibllController {
  //--------------------------------------------------------------------------------------------------------------------/GET ALL
 //--------------------------------------------------------------------------------------------------------------------

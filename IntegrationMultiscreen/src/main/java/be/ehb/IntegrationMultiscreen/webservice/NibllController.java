ca3524c73877bf9b8ee048a9e4489e4ba06da6a5
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2017-12-19 14:05:55 +0100

    java

--- IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
@@ -10,7 +10,8 @@ import be.ehb.IntegrationMultiscreen.DAO.DeviceDAO;
 import be.ehb.IntegrationMultiscreen.DAO.FavorietenDAO;
 import be.ehb.IntegrationMultiscreen.DAO.KamerDAO;
 import be.ehb.IntegrationMultiscreen.DAO.ProfielDAO;
-import be.ehb.IntegrationMultiscreen.DAO.sensorDAO;
+
+import be.ehb.IntegrationMultiscreen.DAO.SensorDAO;
 import be.ehb.IntegrationMultiscreen.model.Activiteit;
 import be.ehb.IntegrationMultiscreen.model.Device;
 import be.ehb.IntegrationMultiscreen.model.Favoriet;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 //import org.springframework.util.MultiValueMap;
 //import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,7 +41,7 @@ public class NibllController {
     }
     @RequestMapping("/getAllSensor")
     public ArrayList<Sensor> getAllSensor(){
-       ArrayList<Sensor> alleActiviteiten = sensorDAO.getSensor();
+       ArrayList<Sensor> alleActiviteiten = SensorDAO.getSensor();
        return alleActiviteiten;
     }@RequestMapping("/getAllDevice")
     public ArrayList<Device> getAllDevice(){
@@ -61,7 +63,61 @@ public class NibllController {
 //--------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------/getById
 //--------------------------------------------------------------------------------------------------------------------
-    
+ @RequestMapping("/getActiviteitById")
+	public Activiteit getActiviteitById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Activiteit activiteit = ActiviteitenDAO.getActiviteitById(id);
+		return activiteit;
+	}
+        
+        @RequestMapping("/getSensorById")
+	public Sensor getSensorById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Sensor sensor = SensorDAO.getSensorById(id);
+		return sensor;
+	}
+        @RequestMapping("/getDeviceById")
+	public Device getDeviceById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Device device = DeviceDAO.getDeviceById(id);
+		return device;
+	}
+        @RequestMapping("/getFavorietentById")
+	public Favoriet getFavorietById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Favoriet favoriet = FavorietenDAO.getFavorietById(id);
+		return favoriet;
+	}
+        @RequestMapping("/getKamerById")
+	public Kamer getKamerById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Kamer kamer = KamerDAO.getKamerById(id);
+		return kamer;
+	}
+        @RequestMapping("/getProfielById")
+	public Profiel getProfielById(@RequestParam(value = "id", defaultValue = "0") int id) {
+		// Defaults aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn
+		// Custom aanroepen met
+		// http://localhost:8080/metEnkelvoudigeGetParameterEnEenvoudigeReturn?naam=Maarten
+                                    Profiel profiel = ProfielDAO.getProfielById(id);
+		return profiel;
+        }   
     
 
 //--------------------------------------------------------------------------------------------------------------------

eaef90137f356000c1ca35b4b728b77a818eccf5
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2017-12-19 12:14:17 +0100

    java

--- IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/webservice/NibllController.java
@@ -6,7 +6,17 @@
 package be.ehb.IntegrationMultiscreen.webservice;
 
 import be.ehb.IntegrationMultiscreen.DAO.ActiviteitenDAO;
+import be.ehb.IntegrationMultiscreen.DAO.DeviceDAO;
+import be.ehb.IntegrationMultiscreen.DAO.FavorietenDAO;
+import be.ehb.IntegrationMultiscreen.DAO.KamerDAO;
+import be.ehb.IntegrationMultiscreen.DAO.ProfielDAO;
+import be.ehb.IntegrationMultiscreen.DAO.sensorDAO;
 import be.ehb.IntegrationMultiscreen.model.Activiteit;
+import be.ehb.IntegrationMultiscreen.model.Device;
+import be.ehb.IntegrationMultiscreen.model.Favoriet;import be.ehb.IntegrationMultiscreen.model.Kamer;
+import be.ehb.IntegrationMultiscreen.model.Profiel;
+s
+import be.ehb.IntegrationMultiscreen.model.Sensor;
 import java.util.ArrayList;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,31 +29,36 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Lennert
  */
 public class NibllController {
-
+ //--------------------------------------------------------------------------------------------------------------------/GET ALL
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
 	@RequestMapping("/getAllActiviteit")
-    public ArrayList<Activiteit> getAll(){
+    public ArrayList<Activiteit> getAllActiviteit(){
        ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
        return alleActiviteiten;
     }
     @RequestMapping("/getAllSensor")
-    public ArrayList<Activiteit> getAll(){
-       ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
+    public ArrayList<Sensor> getAllSensor(){
+       ArrayList<Sensor> alleActiviteiten = sensorDAO.getSensor();
        return alleActiviteiten;
     }@RequestMapping("/getAllDevice")
-    public ArrayList<Activiteit> getAll(){
-       ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
-       return alleActiviteiten;
+    public ArrayList<Device> getAllDevice(){
+       ArrayList<Device> alleDevice = DeviceDAO.getDevice();
+       return alleDevice;
     }@RequestMapping("/getAllFavorieten")
-    public ArrayList<Activiteit> getAll(){
-       ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
-       return alleActiviteiten;
-    }@RequestMapping("/getAllActiviteit")
-    public ArrayList<Activiteit> getAll(){
-       ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
-       return alleActiviteiten;
-    }@RequestMapping("/getAllActiviteit")
-    public ArrayList<Activiteit> getAll(){
-       ArrayList<Activiteit> alleActiviteiten = ActiviteitenDAO.getActiviteit();
-       return alleActiviteiten;
+    public ArrayList<Favoriet> getAllFavorieten(){
+       ArrayList<Favoriet> alleFavorieten = FavorietenDAO.getFavorieten();
+       return alleFavorieten;
+    }@RequestMapping("/getAllKamer")
+    public ArrayList<Kamer> getAllKamer(){
+       ArrayList<Kamer> alleKamer = KamerDAO.getKamers();
+       return alleKamer;
+    }@RequestMapping("/getAllProfiel")
+    public ArrayList<Profiel> getAllProfiel(){
+       ArrayList<Profiel> alleProfiel = ProfielDAO.getProfielen();
+       return alleProfiel;
     }
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
 }

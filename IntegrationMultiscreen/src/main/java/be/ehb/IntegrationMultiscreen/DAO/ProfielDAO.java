47e4169a6ffe15067ca29659fc3d41312d6af198
LIONEL\lione <lionel.maes@student.ehb.be>
2017-12-19 10:50:05 +0100

    dao's
    profielDao erbij + private bij e calsses

---
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/DAO/ProfielDAO.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.IntegrationMultiscreen.DAO;
+import be.ehb.IntegrationMultiscreen.model.Profiel;
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author MaartenH
+ */
+public class ProfielDAO {
+    public static ArrayList<Profiel> getProfielen() {
+		ArrayList<Profiel> resultaat = new ArrayList<Profiel>();
+		try {
+			ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from Profiel");
+			if (mijnResultset != null) {
+				while (mijnResultset.next()) {
+					Profiel huidigeSnoep = converteerHuidigeRijNaarObject(mijnResultset);
+					resultaat.add(huidigeSnoep);
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+
+		return resultaat;
+	}
+
+	public static Profiel getProfielById(int id) {
+		Profiel resultaat = null;
+		try {
+			ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from Profiel where profielId = ?", new Object[] { id });
+			if (mijnResultset != null) {
+				mijnResultset.first();
+				resultaat = converteerHuidigeRijNaarObject(mijnResultset);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+
+		return resultaat;
+	}
+
+	public static int voegProfielToe(Profiel nieuweProfiel) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("INSERT INTO Profiel (profielId, naamProfiel) VALUES (?,?)", new Object[] { nieuweProfiel.getProfielId(),nieuweProfiel.getNaamProfiel()});
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	public static int updateProfiel(Profiel nieuweProfiel) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("UPDATE Profiel SET profielId = ?,naamProfiel = ? WHERE profielId = ?", new Object[] { nieuweProfiel.getProfielId(), nieuweProfiel.getNaamProfiel()});
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	public static int verwijderProfiel(int favorietId) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("DELETE FROM Profiel WHERE ProfielId = ?", new Object[] { favorietId });
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	private static Profiel converteerHuidigeRijNaarObject(ResultSet mijnResultset) throws SQLException {
+		return new Profiel(mijnResultset.getInt("profielId"), mijnResultset.getString("naamProfiel"));
+	}
+}

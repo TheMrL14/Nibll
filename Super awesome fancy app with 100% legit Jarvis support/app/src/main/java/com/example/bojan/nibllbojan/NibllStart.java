1b51eee3d96ba3c16c99b6c7de11e52bae15900b
BojanVD <bojan.vd@gmail.com>
2017-12-19 10:17:14 +0100

    Bojan App Toevoegen

---
+++ Super awesome fancy app with 100% legit Jarvis support/app/src/main/java/com/example/bojan/nibllbojan/NibllStart.java
@@ -0,0 +1,13 @@
+package com.example.bojan.nibllbojan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NibllStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nibll_start);
+    }
+}

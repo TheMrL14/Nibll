7ee3320516cfc7824b5405b4451feb0bbc9143b7
LIONEL\lione <lionel.maes@student.ehb.be>
2017-12-20 14:45:37 +0100

    re

---
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/DAO/ActiviteitenDeviceEnOutputDAO.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.IntegrationMultiscreen.DAO;
+
+import be.ehb.IntegrationMultiscreen.viewModel.ActiviteitenEnKamer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lennert
+ */
+public class ActiviteitenDeviceEnOutputDAO {
+    public static ArrayList<ActiviteitenEnKamer> getActiviteitenEnKamer() {
+		ArrayList<ActiviteitenEnKamer> resultaat = new ArrayList<ActiviteitenEnKamer>();
+		try {
+			ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from IP1718004.ActiviteitenEnKamerenEnKamer");
+			if (mijnResultset != null) {
+				while (mijnResultset.next()) {
+					ActiviteitenEnKamer huidigeActiviteitenEnKamer = converteerHuidigeRijNaarObject(mijnResultset);
+					resultaat.add(huidigeActiviteitenEnKamer);
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+
+		return resultaat;
+	}
+
+	public static ActiviteitenEnKamer getActiviteitenEnKamerById(int id) {
+		ActiviteitenEnKamer resultaat = null;
+		try {
+			ResultSet mijnResultset = Database.voerSqlUitEnHaalResultaatOp("SELECT * from IP1718004.ActiviteitenEnKamerenEnKamer where activiteitenEnKamerId = ?", new Object[] { id });
+			if (mijnResultset != null) {
+				mijnResultset.first();
+				resultaat = converteerHuidigeRijNaarObject(mijnResultset);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+
+		return resultaat;
+	}
+
+	public static int voegActiviteitenEnKamerToe(ActiviteitenEnKamer nieuweActiviteitenEnKamer) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("INSERT INTO IP1718004.ActiviteitenEnKamerenEnKamer (kamerId, activiteitId,) VALUES (?,?)", new Object[] { nieuweActiviteitenEnKamer.getKamerId(),  nieuweActiviteitenEnKamer.getActiviteitenId()});
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	public static int updateActiviteitenEnKamer(ActiviteitenEnKamer nieuweActiviteitenEnKamer) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("UPDATE IP1718004.ActiviteitenEnKamerenEnKamer SET kamerId = ?,  activiteitId = ? WHERE activiteitenEnKamerId = ?", new Object[] {  nieuweActiviteitenEnKamer.getKamerId(),  nieuweActiviteitenEnKamer.getActiviteitenId()});
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	public static int verwijderActiviteitenEnKamer(int activiteitId) {
+		int aantalAangepasteRijen = 0;
+		try {
+			aantalAangepasteRijen = Database.voerSqlUitEnHaalAantalAangepasteRijenOp("DELETE FROM IP1718004.ActiviteitenEnKamerenEnKamer WHERE activiteitenEnKamerId = ?", new Object[] { activiteitId });
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			// Foutafhandeling naar keuze
+		}
+		return aantalAangepasteRijen;
+	}
+
+	private static ActiviteitenEnKamer converteerHuidigeRijNaarObject(ResultSet mijnResultset) throws SQLException {
+		return new ActiviteitenEnKamer(mijnResultset.getInt("activiteitenEnKamerId"), mijnResultset.getInt("kamerId"), mijnResultset.getInt("activiteitId"));
+	}
+}
--- IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/viewModel/KamerEnSensor.java
+++ IntegrationMultiscreen/src/main/java/be/ehb/IntegrationMultiscreen/viewModel/KamerEnSensor.java
@@ -10,5 +10,37 @@ package be.ehb.IntegrationMultiscreen.viewModel;
  * @author lione
  */
 public class KamerEnSensor {
-    
+    private int idKamerEnSensor;
+    private int kamerId;
+    private int sensorId;
+
+    public int getIdKamerEnSensor() {
+        return idKamerEnSensor;
+    }
+
+    public void setIdKamerEnSensor(int idKamerEnSensor) {
+        this.idKamerEnSensor = idKamerEnSensor;
+    }
+
+    public int getKamerId() {
+        return kamerId;
+    }
+
+    public void setKamerId(int kamerId) {
+        this.kamerId = kamerId;
+    }
+
+    public int getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(int sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public KamerEnSensor(int idKamerEnSensor, int kamerId, int sensorId) {
+        this.idKamerEnSensor = idKamerEnSensor;
+        this.kamerId = kamerId;
+        this.sensorId = sensorId;
+    }
 }

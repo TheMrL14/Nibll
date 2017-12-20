8b52f1d3301921404a1f1bef21a8b30005d67282
BojanVD <bojan.vd@gmail.com>
2017-12-20 09:54:19 +0100

    Gridlayout met imagebuttons

--- NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/home.java
+++ NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/home.java
@@ -2,6 +2,8 @@ package com.example.bojan.nibllbojanactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
 

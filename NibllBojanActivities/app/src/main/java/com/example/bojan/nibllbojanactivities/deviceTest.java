12d9a055d24087cf8f0af56c7c9ee94b62217d6c
Say my name <ninjawulf98@gmail.com>
2018-01-08 14:06:00 +0100

    Merge branch 'master' into yolo

--- NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/deviceTest.java
+++ NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/deviceTest.java
@@ -2,18 +2,22 @@ package com.example.bojan.nibllbojanactivities;
 
 
 
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+<<<<<<< HEAD
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
+=======
+>>>>>>> parent of 5ef08e2... somebody's going to die
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,27 +27,51 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.bojan.nibllbojanactivities.services.MyService;
+import com.example.bojan.nibllbojanactivities.utils.Apicall;
 import com.example.bojan.nibllbojanactivities.utils.NetworkHelper;
+import com.example.bojan.nibllbojanactivities.utils.RequestPackage;
+import com.example.bojan.nibllbojanactivities.utils.ResponseApi;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.OkHttpClient;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
 
 public class deviceTest extends AppCompatActivity {
     private static final String urlOff =
+<<<<<<< HEAD
             "http://192.168.1.128:5001/send?protocol=kaku_switch&id=27672578&unit=0&off=1";
-    private static final String urlOn = http://192.168.1.128:5001/send?protocol=kaku_switch&id=27672578&unit=0&off=1";
+    private static final String urlOn = "http://192.168.1.128:5001/send?protocol=kaku_switch&id=27672578&unit=0&off=1";
+=======
+            "http://nibll.local:5001/send?protocol=kaku_switch&id=5218304&unit=1&repeat=5&off=1";
+    private static final String urlOn = "http://nibll.local:5001/send?protocol=kaku_switch&id=5218304&unit=1&repeat=5&on=1";
+>>>>>>> parent of 5ef08e2... somebody's going to die
     private boolean networkOk;
     private boolean ledOn = false;
     TextView output, mTextView;
 
-    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String message =
-                    intent.getStringExtra(MyService.MY_SERVICE_PAYLOAD);
-            output.append(message + "\n");
-        }
-    };
+    private ProgressDialog pDialog;
+    private String path = urlOn;
+    OkHttpClient client;
+    String response;
+    private TextView tvDisc;
+    private ResponseApi[] responseApis;
+
+//    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            String message =
+//                    intent.getStringExtra(MyService.MY_SERVICE_PAYLOAD);
+//            output.append(message + "\n");
+//        }
+//    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,9 +79,9 @@ public class deviceTest extends AppCompatActivity {
 
         output = (TextView) findViewById(R.id.output);
 
-        LocalBroadcastManager.getInstance(getApplicationContext())
-                .registerReceiver(mBroadcastReceiver,
-                        new IntentFilter(MyService.MY_SERVICE_MESSAGE));
+//        LocalBroadcastManager.getInstance(getApplicationContext())
+//                .registerReceiver(mBroadcastReceiver,
+//                        new IntentFilter(MyService.MY_SERVICE_MESSAGE));
 
         networkOk = NetworkHelper.hasNetworkAccess(this);
         output.append("Network ok: " + networkOk);
@@ -68,36 +96,167 @@ public class deviceTest extends AppCompatActivity {
         });
     }
 
+    private class PostDataTOServer extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pDialog = new ProgressDialog(deviceTest.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+
+                client = new OkHttpClient();
+
+                response = Apicall.GET(client, path);
+
+                Gson gson = new Gson();
+                Type type = new TypeToken<Collection<ResponseApi>>() {}.getType();
+                Collection<ResponseApi> enums = gson.fromJson(response, type);
+                responseApis = enums.toArray(new ResponseApi[enums.size()]);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            tvDisc.setText(responseApis[0].getMessage());
+        }
+    }
+
+
+//    public void initializeDiscoveryListener() {
+//
+//
+//        // Instantiate a new DiscoveryListener
+//       NsdManager mDiscoveryListener = new NsdManager.DiscoveryListener() {
+//
+//
+//
+//           public static final String TAG = "lol";
+//
+//           // Called as soon as service discovery begins.
+//            @Override
+//            public void onDiscoveryStarted(String regType) {
+//                Log.d(TAG, "Service discovery started");
+//            }
+//
+//            @Override
+//            public void onServiceFound(NsdServiceInfo service) {
+//                // A service was found! Do something with it.
+//                Log.d(TAG, "Service discovery success" + service);
+////                if (!service.getServiceType().equals(SERVICE_TYPE)) {
+////                    // Service type is the string containing the protocol and
+////                    // transport layer for this service.
+////                    Log.d(TAG, "Unknown Service Type: " + service.getServiceType());
+////                } else if (service.getServiceName().equals(mServiceName)) {
+////                    // The name of the service tells the user what they'd be
+////                    // connecting to. It could be "Bob's Chat App".
+////                    Log.d(TAG, "Same machine: " + mServiceName);
+////                } else if (service.getServiceName().contains("NsdChat")){
+////                    mNsdManager.resolveService(service, mResolveListener);
+////                }
+//            }
+//
+//            @Override
+//            public void onServiceLost(NsdServiceInfo service) {
+//                // When the network service is no longer available.
+//                // Internal bookkeeping code goes here.
+//                Log.e(TAG, "service lost" + service);
+//            }
+//
+//            @Override
+//            public void onDiscoveryStopped(String serviceType) {
+//                Log.i(TAG, "Discovery stopped: " + serviceType);
+//            }
+//
+//            @Override
+//            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+//                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+////                mNsdManager.stopServiceDiscovery(this);
+//            }
+//
+//            @Override
+//            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+//                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+//                mNsdManager.stopServiceDiscovery(this);
+//            }
+//        };
+//    }
+
+//    public void initializeResolveListener() {
+//
+//       NsdManager mResolveListener = new NsdManager.ResolveListener() {
+//           public static final String TAG = "lol";
+//
+//            @Override
+//            public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+//                // Called when the resolve fails. Use the error code to debug.
+//                Log.e(TAG, "Resolve failed" + errorCode);
+//            }
+//
+//            @Override
+//            public void onServiceResolved(NsdServiceInfo serviceInfo) {
+//                Log.e(TAG, "Resolve Succeeded. " + serviceInfo);
+//
+//                if (serviceInfo.getServiceName().equals(mServiceName)) {
+//                    Log.d(TAG, "Same IP.");
+//                    return;
+//                }
+//                NsdServiceInfo mService = serviceInfo;
+//                int port = mService.getPort();
+//                InetAddress host = mService.getHost();
+//            }
+//        };
+//    }
+
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        LocalBroadcastManager.getInstance(getApplicationContext())
-                .unregisterReceiver(mBroadcastReceiver);
+//        LocalBroadcastManager.getInstance(getApplicationContext())
+//                .unregisterReceiver(mBroadcastReceiver);
     }
 
+<<<<<<< HEAD
     public void remoteOn(View view) {
         if(networkOk){
-        Intent intent = new Intent(this, MyService.class);
+//        Intent intent = new Intent(this, MyService.class);
 //                intent.setData(Uri.parse(urlOn));
 //                startService(intent);
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlOn,
-                new Response.Listener<String>() {
-
-                    @Override
-                    public void onResponse(String response) {
-                    }
-                }, new Response.ErrorListener() {
-            public void onErrorResponse(VolleyError error) {
-                Log.i("fuck you all!!!!", "onErrorResponse: didn't work");
-            }
-        });
+        tvDisc = (TextView) findViewById(R.id.output);
+            new PostDataTOServer().execute();
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//
+//// Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlOn,
+//                new Response.Listener<String>() {
+//
+//                    @Override
+//                    public void onResponse(String response) {
+//                    }
+//                }, new Response.ErrorListener() {
+//            public void onErrorResponse(VolleyError error) {
+//                Log.i("fuck you all!!!!", "onErrorResponse: didn't work");
+//            }
+//        });
 // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+//        queue.add(stringRequest);
         Toast.makeText(this, "Led is aan", Toast.LENGTH_SHORT).show();
     }
 
@@ -135,6 +294,8 @@ public class deviceTest extends AppCompatActivity {
 
     }
 
+=======
+>>>>>>> parent of 5ef08e2... somebody's going to die
     public void runClickHandler(View view) {
 
         if (networkOk) {
@@ -143,22 +304,27 @@ public class deviceTest extends AppCompatActivity {
 //                intent.setData(Uri.parse(urlOff));
 //                startService(intent);
                 // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(this);
+                RequestPackage requestPackage = new RequestPackage();
+                requestPackage.setEndPoint(urlOn);
+                requestPackage.setParam("category", "Entrees");
+                requestPackage.setMethod("GET");
 
-// Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlOff,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i("fuck you all!!!!", "onErrorResponse: didn't work");
-                    }
-                });
-// Add the request to the RequestQueue.
-                queue.add(stringRequest);
+//                RequestQueue queue = Volley.newRequestQueue(this);
+//
+//// Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlOff,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.i("fuck you all!!!!", "onErrorResponse: didn't work");
+//                    }
+//                });
+//// Add the request to the RequestQueue.
+//                queue.add(stringRequest);
                 Toast.makeText(this, "Led is uit", Toast.LENGTH_SHORT).show();
             }else{
 //                Intent intent = new Intent(this, MyService.class);
--- NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/utils/HttpHelper.java
+++ NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/utils/HttpHelper.java
@@ -1,82 +1,43 @@
 package com.example.bojan.nibllbojanactivities.utils;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * Helper class for working with a remote server
- */
 public class HttpHelper {
 
-    /**
-     * Returns text from a URL on a web server
-     *
-     * @param address
-     * @return
-     * @throws IOException
-     */
-    public static String downloadUrl(String address) throws IOException {
+    public static String downloadFromFeed(RequestPackage requestPackage)
+            throws IOException {
 
-        InputStream is = null;
-        try {
+        String address = requestPackage.getEndpoint();
+        String encodedParams = requestPackage.getEncodedParams();
 
-            URL url = new URL(address);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            conn.connect();
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode != 200) {
-                throw new IOException("Got response code " + responseCode);
-            }
-            is = conn.getInputStream();
-            return readStream(is);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (is != null) {
-                is.close();
-            }
+        if (requestPackage.getMethod().equals("GET") &&
+                encodedParams.length() > 0) {
+            address = String.format("%s?%s", address, encodedParams);
         }
-        return null;
-    }
 
-    /**
-     * Reads an InputStream and converts it to a String.
-     *
-     * @param stream
-     * @return
-     * @throws IOException
-     */
-    private static String readStream(InputStream stream) throws IOException {
+        OkHttpClient client = new OkHttpClient();
 
-        byte[] buffer = new byte[1024];
-        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-        BufferedOutputStream out = null;
-        try {
-            int length = 0;
-            out = new BufferedOutputStream(byteArray);
-            while ((length = stream.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
-            }
-            out.flush();
-            return byteArray.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (out != null) {
-                out.close();
-            }
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(address);
+
+        Request request = requestBuilder.build();
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Exeption: response code " + response.code());
         }
-    }
 
+
+    }
 }

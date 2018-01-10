39e8373e59032e29875e5e1f41ac26681032e070
Say my name <ninjawulf98@gmail.com>
2018-01-10 12:15:55 +0100

    Auto stash before merge of "master" and "origin/master"

--- NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/scanScreen.java
+++ NibllBojanActivities/app/src/main/java/com/example/bojan/nibllbojanactivities/scanScreen.java
@@ -1,15 +1,24 @@
 package com.example.bojan.nibllbojanactivities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.bojan.nibllbojanactivities.utils.MySingleton;
+
 public class scanScreen extends AppCompatActivity {
 
     ImageButton androidImageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +29,26 @@ public class scanScreen extends AppCompatActivity {
 
         androidImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { //Als er op de knop geklikt word, word er een geluid afgespeeld op zowel de gsm als de raspberry pi en wordt de device pagina geopend
                 Toast.makeText(scanScreen.this, "Nibll was found", Toast.LENGTH_LONG).show();
                 Intent intentLoadNewActivity = new Intent(scanScreen.this, home.class);
+                String url ="http://192.168.1.128:5000/play/plop.mp3";
+
+                final MediaPlayer mediaPlayer = MediaPlayer.create(scanScreen.this, R.raw.plop);
+                mediaPlayer.start();
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(scanScreen.class.toString(), "onErrorResponse: " + error);
+                    }
+                });
+                MySingleton.getInstance(scanScreen.this).addToRequestQueue(stringRequest);
                 startActivity(intentLoadNewActivity);
             }
         });
